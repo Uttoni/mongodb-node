0992bedba4910fd53f447d90eed83862a2b34bd9
Uttoni <uttoni.brandani@ges.inatel.br>
2021-12-24 01:20:18 -0300

    Testing how to insert users

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -4,8 +4,33 @@ import { Product } from '../models/Product';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response)=>{
+/*
+    let newUser = await User.create({
+        name:{
+            firstName: "Diego",
+            lastName:"Carneiro"
+        },
+        email: "diego@gmail.com",
+        age: 20,
+        interests:["games", "hardware", "sugary things"]
+    });
+*/
 
-    let users = await User.find({interests:"books"});
+//BETTER WAY OF INSERTING ------------------------------------------------------------------------
+/*
+    let user = new User();
+    user.name.firstName = "Bruno";
+    user.name.lastName = "Uber";
+    user.email = "bruno@gmail.com";
+    user.age = 19;
+    user.interests = ["tattoos", "gym", "army"];
+    let resultado = await user.save();
+
+    console.log("new user:", resultado);
+*/
+
+
+    let users = await User.find({ age: { $gte: 20} }).sort({ "name.firstName": 1, age: 1 }).skip(0).limit(5);
     console.log("Users:", users);
 
     let age: number = 90;
